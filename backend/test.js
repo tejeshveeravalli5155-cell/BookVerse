8e2d414f88edda4a6bce864a30cb254469d19ee6
tejeshveeravalli5155-cell <tejeshveeravalli5155@gmail.com>
2026-07-16 22:49:00 +0530

    Add backend testing file

---
+++ backend/test.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Connected Successfully");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
