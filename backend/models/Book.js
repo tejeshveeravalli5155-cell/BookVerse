7d04e79f3ab4ec69daac2329579d22352c2a23c1
tejeshveeravalli5155-cell <tejeshveeravalli5155@gmail.com>
2026-07-17 20:41:22 +0530

    Added Mongoose schema validations

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -4,15 +4,26 @@ const bookSchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      required: true,
+      required: [true, "Book title is required"],
+      trim: true,
+      minlength: [3, "Title must be at least 3 characters"],
+      maxlength: [100, "Title cannot exceed 100 characters"],
+      unique: true,
     },
+
     author: {
       type: String,
-      required: true,
+      required: [true, "Author name is required"],
+      trim: true,
+      minlength: [3, "Author name must be at least 3 characters"],
+      maxlength: [50, "Author name cannot exceed 50 characters"],
     },
+
     price: {
       type: Number,
-      required: true,
+      required: [true, "Price is required"],
+      min: [1, "Price must be greater than 0"],
+      max: [10000, "Price cannot exceed 10000"],
     },
   },
   {
