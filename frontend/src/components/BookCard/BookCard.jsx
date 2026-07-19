e2dbbc8cde50f4c6e0920d0aaa164d3bf3ec2365
tejeshveeravalli5155-cell <tejeshveeravalli5155@gmail.com>
2026-07-19 20:43:47 +0530

    Updated book card with image support

--- frontend/src/components/BookCard/BookCard.jsx
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -8,88 +8,60 @@ function BookCard({
   author,
   price,
   image,
-  favorite,
-  onFavorite,
   onDelete,
-  isLocal,
 }) {
-
-  function saveRecentlyViewed() {
-
-    let recent =
-      JSON.parse(localStorage.getItem("recentBooks")) || [];
-
-    recent = recent.filter((book) => book.id !== id);
-
-    recent.unshift({
-      id,
-      title,
-      author,
-      image,
-    });
-
-    recent = recent.slice(0, 5);
-
-    localStorage.setItem(
-      "recentBooks",
-      JSON.stringify(recent)
-    );
-  }
-
+  console.log(image);
   return (
     <div className="book-card">
 
-      <img
-        src={image}
-        alt={title}
-      />
+     <img
+  src={
+    image && image.trim() !== ""
+      ? image
+      : "https://placehold.co/180x250?text=No+Cover"
+  }
+  alt={title}
+  className="book-image"
+/>
 
       <div className="book-content">
-
         <h3>{title}</h3>
 
         <p>
           <strong>Author:</strong> {author}
         </p>
 
-        <h4>{price}</h4>
-
+        <h4>₹{price}</h4>
       </div>
 
       <div className="button-group">
 
         <Button text="Buy Now" />
 
-        <Link
-          to={`/details/${id}`}
-          onClick={saveRecentlyViewed}
-        >
+        <Link to={`/details/${id}`}>
           <button className="view-btn">
             View Details
           </button>
         </Link>
 
-        {isLocal && (
-          <Link to={`/edit-book/${id}`}>
-            <button className="edit-btn">
-              ✏ Edit
-             </button>
-          </Link>
-        )}
-
-        <button
-          className="favorite-btn"
-          onClick={onFavorite}
-        >
-          {favorite ? "❤️" : "🤍"}
-        </button>
+        <Link to={`/edit-book/${id}`}>
+          <button className="edit-btn">
+            ✏ Edit
+          </button>
+        </Link>
 
         <button
           className="delete-btn"
           onClick={onDelete}
         >
-          Delete
+          🗑 Delete
         </button>
+        
+<Link to={`/read/${id}`}>
+    <button className="read-btn">
+        📖 Read Book
+    </button>
+</Link>
 
       </div>
 
