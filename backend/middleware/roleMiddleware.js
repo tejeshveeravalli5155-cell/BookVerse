8ab8a6c17a7b69d782de4dc259150d270f451346
tejeshveeravalli5155-cell <tejeshveeravalli5155@gmail.com>
2026-07-23 23:26:34 +0530

    Add role based access middleware

---
+++ backend/middleware/roleMiddleware.js
@@ -0,0 +1,18 @@
+// ==========================
+// Role Based Authorization
+// ==========================
+const roleMiddleware = (...roles) => {
+  return (req, res, next) => {
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "Access Denied. Insufficient Permissions.",
+      });
+    }
+
+    next();
+  };
+};
+
+export default roleMiddleware;
